786b8ffe6f35c8d9138f04c8d411cd2c67008773
sagor <sagor.eee.cuet@gmail.com>
2024-04-29 02:54:54 -0500

    sls event generate and lambda local test

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # aws-serverless
 AWS With Serverless Framework
+
+## lambda invoke
+```bash
+# With event file
+sls invoke -f functionName -p path/of/event-json/file
+# With data
+sls invoke -f create -d '{"body":"Some meaningless data"}'
+```
+
+## lambda invoke local
+```bash
+# With event file
+sls invoke local -f functionName -p path/of/event-json/file
+# With data
+sls invoke local -f create -d '{"body":"Some meaningless data"}'
+```
+
+## lambda deploy
+```bash
+sls deploy --verbose
+sls deploy --stage stage-name --region region-name --function function-name --verbose
+```
+
+## generate event
+```bash
+sls generate-event -t aws:apiGateway > test-event.json
+```
+
+## display information about deployed service
+```bash
+sls info
+``` 
+
+## package your entire infrastructure
+```bash
+sls package
+```
--- TodosServerless/todos/create/create.py
+++ TodosServerless/todos/create/create.py
@@ -1,41 +1,29 @@
-import json
-import logging
-import os
 import time
 import uuid
-
 import boto3
 
 # dynamodb = boto3.resource('dynamodb')
 
-
 def create(event, context):
-    print("Lambda Invoked")
-    data = json.loads(event['body'])
-
-    # if 'text' not in data:
-    #     logging.error("Validation Failed")
-    #     raise Exception("Couldn't create the todo item.")
-    
-    # timestamp = str(time.time())
-
-    # table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
-    # item = {
-    #     'id': str(uuid.uuid1()),
-    #     'text': data['text'],
-    #     'checked': False,
-    #     'createdAt': timestamp,
-    #     'updatedAt': timestamp,
-    # }
+    print("Lambda Invoked\n")
+    data = event['body']
+    timestamp = str(time.time())
+    item = {
+        'id': str(uuid.uuid1()),
+        'data': data,
+        'checked': False,
+        'createdAt': timestamp,
+        'updatedAt': timestamp,
+    }
 
     # # write the todo to the database
+    # table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     # table.put_item(Item=item)
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(data)
+        "body": item
     }
 
     return response
